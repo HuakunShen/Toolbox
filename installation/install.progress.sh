85645407d48f30a13afe3eff5a36239e46c54b14
Huakun Shen <33727687+HuakunShen@users.noreply.github.com>
2021-06-12 05:09:51 -0400

    Update install.progress.sh

--- installation/install.progress.sh
+++ installation/install.progress.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-install_dirname="install_progress_$RANDOM"
-mkdir $install_dirname
-cd $install_dirname
 wget https://raw.githubusercontent.com/HuakunShen/Toolbox/master/bin/progress
 wget https://github.com/HuakunShen/Toolbox/blob/master/man/progress.1.gz?raw=true --output-document=progress.1.gz
 sudo mv ./progress.1.gz /usr/share/man/man1
 chmod a+x ./progress
 sudo mv ./progress /usr/local/bin
-cd ..
-rmdir $install_dirname
+rm ./progress
+rm ./progress.1.gz
+
 echo done
