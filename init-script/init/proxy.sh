66564f53fd95daa38010f903a44fa73ac322ab98
Huakun Shen <33727687+HuakunShen@users.noreply.github.com>
2021-08-12 17:19:14 +0800

    Update proxy.sh

--- init-script/init/proxy.sh
+++ init-script/init/proxy.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+export proxy_server_address=localhost;
+export proxy_server_port=7890;
 start_proxy() {
-	export http_proxy=http://localhost:8889;
-	export https_proxy=http://localhost:8889;
-	echo "set http and https proxy to localhost:8889";
+	export http_proxy="http://$proxy_server_address:$proxy_server_port";
+	export https_proxy="http://$proxy_server_address:$proxy_server_port";
+	export all_proxy="http://$proxy_server_address:$proxy_server_port";
 }
 
-end_proxy() {
+stop_proxy() {
 	unset http_proxy;
 	unset https_proxy;
+	unset all_proxy;
+}
+
+start_git_proxy() {
+	git config --global http.proxy "http://$proxy_server_address:$proxy_server_port";
+	git config --global https.proxy "http://$proxy_server_address:$proxy_server_port";
+}
+
+stop_git_proxy() {
+	git config --global --unset https.proxy;
+	git config --global --unset http.proxy;
 }
